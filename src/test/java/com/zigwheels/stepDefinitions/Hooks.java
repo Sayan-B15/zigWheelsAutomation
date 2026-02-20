76da4f0bcf43a1268d674489217a73a85a201205
Sayan-B15 <sayanbhatta151203@gmail.com>
2026-02-20 11:13:50 +0530

    Extent report fix

--- src/test/java/com/zigwheels/stepDefinitions/Hooks.java
+++ src/test/java/com/zigwheels/stepDefinitions/Hooks.java
@@ -2,13 +2,25 @@ package com.zigwheels.stepDefinitions;
 
 import com.zigwheels.base.BaseClass;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks extends BaseClass {
 
     @Before
     public void setup() {
-        initialization(); // This will trigger the JOption dialog box
+        initialization();
+    }
+
+    // This method will attach a screenshot to every step in the report
+    @AfterStep
+    public void addScreenshot(Scenario scenario) {
+        // You can also wrap this in 'if(scenario.isFailed())' if you only want failure shots
+        final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", "Step Status");
     }
 
     @After
